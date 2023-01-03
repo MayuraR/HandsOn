1be7d37d23462885de58dc52836fad810f52bd5b
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-03 19:20:09 +0530

    changes for chapter 9

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-// creating a server using express
 const express = require('express');
 const app = express();
 
+// uncomment each line and use nodemon 
+// command for nodemon: nodemon app.js
+app.get('/', (req, res) =>{
+    res.send("HOME!");
+    //res.send('<h1>HOME!</h1>')
+    // res.sendStatus(500)
+    // res.status(500).send("Error")
+    // res.status(500).json({'message': 'error'})
+
+    // res.download('./app.js')
+})
+
 
 app.listen(3000);
-
-
-// this will not return any response from the server
-// when we will send a request to local host, we will get an error
