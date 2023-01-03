2627755938e2b1ff9e8092ae9ba34e4e2a6a60ff
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-03 19:22:38 +0530

    changes for chapter 10

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
 
-// uncomment each line and use nodemon 
-// command for nodemon: nodemon app.js
+// rendering an html file
 app.get('/', (req, res) =>{
-    res.send("HOME!");
-    //res.send('<h1>HOME!</h1>')
-    // res.sendStatus(500)
-    // res.status(500).send("Error")
-    // res.status(500).json({'message': 'error'})
-
-    // res.download('./app.js')
+    res.sendFile('./views/index.html', {root: __dirname})
 })
 
 
