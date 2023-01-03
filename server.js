b7b65d19b0ec02802d39f5d83f54e7c81d660ca2
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-03 18:32:26 +0530

    changes for chapter 3

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const http = require('http');
 // create a server
 const server = http.createServer((req, res) => {  
     console.log('Request Made')
-    // sending text in the response and setting the header
-    res.setHeader('Content-Type', 'text/plain')
-    res.write("HOME");
+    // sending html in the response and setting the header
+    res.setHeader('Content-Type', 'text/html')
+    res.write("<h1>HOME</h1>");
     res.end();
 });
 
