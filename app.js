438ef30a25d03e8e8211328437e8d9f2c84a67ef
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-03 19:25:20 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
-// rendering an html file
+// routing
 app.get('/', (req, res) =>{
     res.sendFile('./views/index.html', {root: __dirname})
 })
 
+app.get('/about', (req, res) =>{
+    res.sendFile('./views/contactUs.html', {root: __dirname});
+})
+
+// where you place app.use is very important
+app.use((req, res) =>{
+    res.status(404).sendFile('./views/error.html', {root: __dirname});
+})
 
 app.listen(3000);
