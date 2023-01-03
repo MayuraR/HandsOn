ddd3155b92b50b4407f3e0436cb2030ac9f8da34
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-03 23:02:34 +0530

    chapter 13

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
-// redirect
+// set view engine used
+// we will be using ejs so step 1: install ejs
+// Step 2: set view engine 
+
+app.set('view engine','ejs');
+// Step 3: rename .html to .ejs
+
+// app.set('views', 'data')
+
 app.get('/', (req, res) =>{
-    res.sendFile('./views/index.html', {root: __dirname})
+    // res.sendFile('./views/index.html', {root: __dirname})
+    // res.render('index')
+    // step 2:
+    res.render('index', {text : "VISIT US"})
 })
 
 app.get('/about', (req, res) =>{
-    res.sendFile('./views/contactUs.html', {root: __dirname});
+    // res.sendFile('./views/contactUs.html', {root: __dirname});
+    res.render('contactUs', {text123 : "VISIT US"})
 })
 
 app.get('/contact', (req, res) =>{
@@ -16,7 +28,8 @@ app.get('/contact', (req, res) =>{
 
 // where you place app.use is very important
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/error.html', {root: __dirname});
+    // res.status(404).sendFile('./views/error.html', {root: __dirname});
+    res.status(404).render('error')
 })
 
 app.listen(3000);
