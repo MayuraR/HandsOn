8e0ff7f4c9e99957be9313b4389ef9ca8a29b5ac
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2023-01-04 06:49:11 +0530

    middleware

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const app = express();
 
-// set view engine used
-// we will be using ejs so step 1: install ejs
-// Step 2: set view engine 
-
 app.set('view engine','ejs');
-// Step 3: rename .html to .ejs
 
-// app.set('views', 'data')
+app.use((req, res, next) => {
+    console.log('Inside middleware')
+    console.log(req.method);
+    next();
+})
 
 app.get('/', (req, res) =>{
-    // res.sendFile('./views/index.html', {root: __dirname})
-    // res.render('index')
-    // step 2:
     res.render('index', {text : "VISIT US"})
 })
 
 app.get('/about', (req, res) =>{
-    // res.sendFile('./views/contactUs.html', {root: __dirname});
     res.render('contactUs', {text123 : "VISIT US"})
 })
 
@@ -26,9 +21,7 @@ app.get('/contact', (req, res) =>{
     res.redirect('/about')
 })
 
-// where you place app.use is very important
 app.use((req, res) =>{
-    // res.status(404).sendFile('./views/error.html', {root: __dirname});
     res.status(404).render('error')
 })
 
